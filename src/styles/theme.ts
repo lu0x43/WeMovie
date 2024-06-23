b877add3e2a785b7b9a3895c53811c324b743700
lu0x43 <lucas_caetano123@hotmail.com>
2024-06-23 01:51:14 -0300

    style: defining base theme project

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import {DefaultTheme} from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  colors: {
+    background: '#2F2E41',
+    textWhite: '#FFFFFF',
+    textBlack: '#333333',
+    textSecondary: '#999999',
+    button: '#009EDD',
+    buttonActive: '#039B00',
+  },
+};
